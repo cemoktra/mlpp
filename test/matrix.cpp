51aac9b3bf60c811618c8f21579b2e0e22c28143
cemoktra <bazz-dee@gmx.de>
2019-11-09 09:43:40 +0100

    added basic iterator sample code

--- test/matrix.cpp
+++ test/matrix.cpp
@@ -24,3 +24,24 @@ TEST_F (matrix_test, add) {
     for (auto i = 0; i < 9; i++)
         ASSERT_EQ(m3.get_at(i / 3, i % 3), 3);
 }
+
+
+
+// class iterator {
+//         long num = FROM;
+//     public:
+//         iterator(long _num = 0) : num(_num) {}
+//         iterator& operator++() {num = TO >= FROM ? num + 1: num - 1; return *this;}
+//         iterator operator++(int) {iterator retval = *this; ++(*this); return retval;}
+//         bool operator==(iterator other) const {return num == other.num;}
+//         bool operator!=(iterator other) const {return !(*this == other);}
+//         long operator*() {return num;}
+//         // iterator traits
+//         using difference_type = long;
+//         using value_type = long;
+//         using pointer = const long*;
+//         using reference = const long&;
+//         using iterator_category = std::forward_iterator_tag;
+//     };
+//     iterator begin() {return FROM;}
+//     iterator end() {return TO >= FROM? TO+1 : TO-1;}
