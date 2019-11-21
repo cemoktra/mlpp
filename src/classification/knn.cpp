7fdbfba534ac55d75ddcdb93b01edee22d0f5c66
Bastian Schubert <b.schubert82@gmail.com>
2019-11-21 15:40:00 +0100

    fixed knn

--- src/classification/knn.cpp
+++ src/classification/knn.cpp
@@ -2,61 +2,47 @@
 #include <algorithm>
 #include <iterator>
 #include <xtensor/xview.hpp>
+#include <xtensor/xio.hpp>
+#include <xtensor/xsort.hpp>
+#include <xtensor/xindex_view.hpp>
 #include <xtensor-blas/xlinalg.hpp>
 
 knn::knn()
     : m_classes(0)
-    , m_kdistances(nullptr)
-    , m_knearest_classes(nullptr)
 {
     register_param("k", 3);
-    m_class_count = nullptr;
-}
-
-knn::~knn()
-{
-    delete [] m_kdistances;
-    delete [] m_knearest_classes;
-    delete [] m_class_count;
 }
 
 xt::xarray<double> knn::predict(const xt::xarray<double>& x)
 {
-    auto shape = x.shape();
-    shape[1] = 1;
-    xt::xarray<double> result (shape);
+    xt::xarray<double> result (std::vector<size_t>( { x.shape()[0], 1 }));
     auto kneighbors = static_cast<size_t>(get_param("k"));
-
+    
     for (auto i = 0; i < x.shape()[0]; i++)
     {
+        m_kdistances.fill(std::numeric_limits<double>::max());
+        m_class_count.fill(0);
+
         xt::xarray<double> x_row = xt::view(x, xt::range(i, i + 1), xt::all());
-        for (auto k = 0; k < kneighbors; k++)
-            m_kdistances[k] = std::numeric_limits<double>::max();
-        memset(m_class_count, 0, m_classes * sizeof(size_t));
-        
+
         for (auto j = 0; j < m_x_train.shape()[0]; j++)
         {
             xt::xarray<double> x_train_row = xt::view(m_x_train, xt::range(j, j + 1), xt::all()) - x_row;
             auto distance = xt::linalg::norm(x_train_row);
+
+            if (distance < m_kdistances(kneighbors - 1)) {
+                 m_kdistances(kneighbors - 1) = distance;
+                 m_knearest_classes(kneighbors - 1) = static_cast<size_t>(m_y_train(j, 0));
+            }
             
-            if (distance < m_kdistances[kneighbors - 1]) {
-                m_kdistances[kneighbors - 1] = distance;
-                m_knearest_classes[kneighbors - 1] = static_cast<size_t>(m_y_train(i, 0));
-            }
-
-            for (auto k = kneighbors - 1; k > 0; k--)
-            {
-                if (m_kdistances[k] < m_kdistances[k - 1]) {
-                    std::swap(m_kdistances[k], m_kdistances[k - 1]);
-                    std::swap(m_knearest_classes[k], m_knearest_classes[k - 1]);
-                }
-            }
+            auto idx = xt::argsort(m_kdistances);
+            m_kdistances = xt::index_view(m_kdistances, idx);
+            m_knearest_classes = xt::index_view(m_knearest_classes, idx);
         }
-
+        
         for (auto k = 0; k < kneighbors; k++)
-            m_class_count[m_knearest_classes[k]]++;
-        auto max_elem = std::max_element(m_class_count, m_class_count + m_classes);
-        result(i, 0) = max_elem - m_class_count;
+            m_class_count(m_knearest_classes(k))++;
+        result(i, 0) = xt::argmax(m_class_count)(0);
     }
 
     return result;
@@ -86,10 +72,9 @@ void knn::train(const xt::xarray<double>& x, const xt::xarray<double>& y)
 {
     auto k = static_cast<size_t>(get_param("k"));
 
-    m_kdistances = new double[k];
-    m_knearest_classes = new size_t[k];
-
-    m_class_count = new size_t[m_classes];
+    m_kdistances       = xt::zeros<double>(std::vector<size_t>({k}));
+    m_knearest_classes = xt::zeros<size_t>(std::vector<size_t>({k}));
+    m_class_count      = xt::zeros<size_t>(std::vector<size_t>({m_classes}));
     m_x_train = x;
     m_y_train = y;
 }
--- src/classification/knn.h
+++ src/classification/knn.h
@@ -9,7 +9,7 @@ class knn : public classifier
 public:
     knn();
     knn(const knn&) = delete;
-    ~knn();
+    ~knn() = default;
 
     xt::xarray<double> predict(const xt::xarray<double>& x) override;
     double score(const xt::xarray<double>& x, const xt::xarray<double>& y) override;
@@ -22,10 +22,14 @@ public:
 private:
     xt::xarray<double> m_x_train;
     xt::xarray<double> m_y_train;
+    xt::xarray<double> m_kdistances;
+    xt::xarray<size_t> m_knearest_classes;
+    xt::xarray<size_t> m_class_count;
+
     size_t m_classes;
-    double *m_kdistances;
-    size_t *m_knearest_classes;
-    size_t *m_class_count;
+    // double *m_kdistances;
+    // size_t *m_knearest_classes;
+    // size_t *m_class_count;
 };
 
 #endif
